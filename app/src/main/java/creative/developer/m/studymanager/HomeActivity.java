8ea35cdfed9056a8b3f01f160ecb3f78d44cfca7
Mohammedgh007 <mohammedgh007@gmail.com>
2019-05-30 18:00:50 -0700

    adding xml file

---
+++ app/src/main/java/creative/developer/m/studymanager/HomeActivity.java
@@ -0,0 +1,20 @@
+/*
+###############################################################################
+
+
+###############################################################################
+ */
+
+package creative.developer.m.studymanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+    }
+}
