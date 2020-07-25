b3c5dbf542fb0b6a1e8c681f39b16e2c44807219
Mohammedgh007 <mohammedgh007@gmail.com>
2020-07-25 14:51:32 -0700

    Finished adding animations for cards

--- app/src/main/java/creative/developer/m/studymanager/modelview/FlashCardsActivity.java
+++ app/src/main/java/creative/developer/m/studymanager/modelview/FlashCardsActivity.java
@@ -15,9 +15,20 @@ Methods:
 
 package creative.developer.m.studymanager.modelview;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.Window;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -79,7 +90,7 @@ public class FlashCardsActivity extends AppCompatActivity {
 
 
         // filling cardTV with the text of the first question.
-        showQuestion();
+        showQuestion("flip");
 
 
         // click event handling which showing the next card question
@@ -89,12 +100,11 @@ public class FlashCardsActivity extends AppCompatActivity {
             // determining the next card number if current is not the last.
             if (currCardIndex + 1 != recievedCards.size()) {
                 currCardIndex++;
+                showQuestion("slide down");
             } else {
-                Toast.makeText(getBaseContext(), "This is the last card; you can press restart"
-                        , Toast.LENGTH_LONG).show();
+                showQuestion("shake");
             }
 
-            showQuestion();
             btn.setClickable(true);
         });
 
@@ -106,11 +116,11 @@ public class FlashCardsActivity extends AppCompatActivity {
             // determining the previous card number if current is not the first.
             if (currCardIndex != 0) {
                 currCardIndex--;
+                showQuestion("slide up");
             } else {
-                Toast.makeText(getBaseContext(), "This is the first card.", Toast.LENGTH_LONG).show();
+                showQuestion("shake");
             }
 
-            showQuestion();
             btn.setClickable(true);
         });
 
@@ -125,12 +135,28 @@ public class FlashCardsActivity extends AppCompatActivity {
 
                 // creating the shown text
                 String showedText = "card " + (currCardIndex + 1) + " out of " + recievedCards.size()
-                        + "\n\n";
-                showedText += recievedCards.get(currCardIndex).getAnswer();
-                cardTV.setText(showedText);
+                        + "\n\n" + recievedCards.get(currCardIndex).getAnswer();
+
+                //preparing animations.
+                final ObjectAnimator oa1 = ObjectAnimator.ofFloat(backgroundCard,
+                        "scaleX", 1f, 0f);
+                final ObjectAnimator oa2 = ObjectAnimator.ofFloat(backgroundCard,
+                        "scaleX", 0f, 1f);
+                oa1.setInterpolator(new DecelerateInterpolator());
+                oa2.setInterpolator(new AccelerateDecelerateInterpolator());
+                oa1.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                        cardTV.setText(showedText);
+                        oa2.start();
+                    }
+                });
+                oa1.start();
+
                 isQuestion = false;
             } else {
-                showQuestion();
+                showQuestion("flip");
             }
 
             btn.setClickable(true);
@@ -143,7 +169,7 @@ public class FlashCardsActivity extends AppCompatActivity {
 
             Collections.shuffle(Arrays.asList(recievedCards));
             currCardIndex = 0;
-            showQuestion();
+            showQuestion("flip");
             isQuestion = true;
 
             btn.setClickable(true);
@@ -189,15 +215,78 @@ public class FlashCardsActivity extends AppCompatActivity {
 
 
     // this method show the question on cardTV
-    private void showQuestion() {
+    // animType is either "flip", "slide up", "slide down", or "shake".
+    private void showQuestion(String animType) {
         // changing the card background
         backgroundCard.setBackgroundResource(R.color.card_question);
 
         // creating the shown text
         String showedText = "card " + (currCardIndex + 1) + " out of " + recievedCards.size()
-                + "\n\n";
-        showedText += recievedCards.get(currCardIndex).getQuestion();
-        cardTV.setText(showedText);
+                + "\n\n" + recievedCards.get(currCardIndex).getQuestion();
+
+        //preparing animations
+        int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
+        if (animType.equals("flip")){ // for flipping the card
+            final ObjectAnimator oa1 = ObjectAnimator.ofFloat(backgroundCard,
+                    "scaleX", 1f, 0f);
+            final ObjectAnimator oa2 = ObjectAnimator.ofFloat(backgroundCard,
+                    "scaleX", 0f, 1f);
+            oa1.setInterpolator(new DecelerateInterpolator());
+            oa2.setInterpolator(new AccelerateDecelerateInterpolator());
+            oa1.setDuration(300);
+            oa2.setDuration(300);
+            oa1.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                    cardTV.setText(showedText);
+                    oa2.start();
+                }
+            });
+            oa1.start();
+        // for going to another card
+        } else if (animType.equals("slide down") || animType.equals("slide up")) {
+            ObjectAnimator translation1;
+            ObjectAnimator translation2;
+            if (animType.equals("slide down")) {
+                translation1 = ObjectAnimator.ofFloat(backgroundCard,
+                        "translationY", 0, -screenHeight);
+                translation2 = ObjectAnimator.ofFloat(backgroundCard,
+                        "translationY", 0);
+            } else {
+                translation1 = ObjectAnimator.ofFloat(backgroundCard,
+                        "translationY", 0, 2 * screenHeight);
+                translation2 = ObjectAnimator.ofFloat(backgroundCard,
+                        "translationY", 0);
+            }
+            translation1.setInterpolator(new DecelerateInterpolator());
+            translation2.setInterpolator(new DecelerateInterpolator());
+            translation1.setDuration(500);
+            translation2.setDuration(3);
+            translation1.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                    backgroundCard.setVisibility(View.INVISIBLE);
+                    translation2.start();
+                }
+            });
+            translation2.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                    cardTV.setText(showedText);
+                    backgroundCard.setVisibility(View.VISIBLE);
+                }
+            });
+            translation1.start();
+        } else { // for showing the user that there's no more cards to the right or left.
+            ObjectAnimator shakeAnim = ObjectAnimator.ofFloat(backgroundCard,
+                    "translationY", 0, screenHeight / 7, 0, -screenHeight / 7, 0);
+            shakeAnim.setInterpolator(new DecelerateInterpolator());
+            shakeAnim.setDuration(500);
+            shakeAnim.start();
+        }
 
         isQuestion = true;
     }
@@ -213,7 +302,7 @@ public class FlashCardsActivity extends AppCompatActivity {
             recievedCards = model.getLessonCards(courseName, lessonName);
             // showing the appropriate card
             currCardIndex = 0;
-            showQuestion();
+            showQuestion("flip");
         }
     }
 
